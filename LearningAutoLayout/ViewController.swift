c1865cad1c1ec7b3b3edfc08a2c83050fd0e9ec3
dilara-s <sagadieva.d2017@yandex.ru>
2026-09-16 18:28:03 +0300

    Add last row

--- LearningAutoLayout/ViewController.swift
+++ LearningAutoLayout/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 private enum Layout {
     static let spacing: CGFloat = 12
     static let sideInset: CGFloat = 16
-    static let textSize: CGFloat = 20
+    static let fontSize: CGFloat = 20
     static let cornerRadius: CGFloat = 40
+    static let bottomInset: CGFloat = 16
 }
 
 class ViewController: UIViewController {
@@ -19,14 +20,31 @@ class ViewController: UIViewController {
         ["AC", "±", "%", "÷"],
         ["7", "8", "9", "×"],
         ["4", "5", "6", "−"],
-        ["1", "2", "3", "+"]
+        ["1", "2", "3", "+"],
     ]
+    
+    let lastRow: [String] = [
+        "0", ",", "="
+    ]
+    
+    lazy var buttonStack: UIStackView = {
+        let buttonStack = UIStackView()
+        for row in titles {
+            buttonStack.addArrangedSubview(makeRow(titles: row))
+        }
+        buttonStack.addArrangedSubview(makeLastRow())
+        buttonStack.axis = .vertical
+        buttonStack.spacing = Layout.spacing
+        buttonStack.distribution = .fillEqually
+        
+        return buttonStack
+    }()
 
     let label: UILabel = {
         let label = UILabel()
         label.text = "0"
         label.textColor = .white
-        label.font = .boldSystemFont(ofSize: Layout.textSize)
+        label.font = .boldSystemFont(ofSize: Layout.fontSize)
         label.backgroundColor = .systemGray
         label.textAlignment = .right
         return label
@@ -56,21 +74,25 @@ class ViewController: UIViewController {
         print("button pressed")
     }
     
-    func makeButton(title: String) -> UIButton {
+    func makeButton(title: String, isSquare: Bool = true) -> UIButton {
         let button = UIButton()
         button.setTitle(title, for: UIControl.State.normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
         
         button.layer.cornerRadius = Layout.cornerRadius
         button.backgroundColor = .systemBlue
+
+        if isSquare {
+            NSLayoutConstraint.activate([
+                button.widthAnchor.constraint(equalTo: button.heightAnchor)
+            ])
+        }
         
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
         return button
     }
     
-    func makeRow(titles: [String]) -> UIStackView{
+    func makeRow(titles: [String]) -> UIStackView {
         let stackView = UIStackView()
         
         for title in titles {
@@ -78,35 +100,38 @@ class ViewController: UIViewController {
         }
         
         stackView.axis = .horizontal
-        
         stackView.distribution = .fillEqually
         stackView.spacing = Layout.spacing
         
         return stackView
     }
-
     
-    func setupButtonStack() {
-        let verticalStackView = UIStackView()
-        
-        for row in titles {
-            verticalStackView.addArrangedSubview(makeRow(titles: row))
+    func makeLastRow() -> UIStackView {
+        let lastRowStackView = UIStackView()
+        for title in lastRow {
+            if title == "0" {
+                lastRowStackView.addArrangedSubview(makeButton(title: title, isSquare: false))
+            } else {
+                lastRowStackView.addArrangedSubview(makeButton(title: title))
+            }
         }
         
-        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(verticalStackView)
+        lastRowStackView.axis = .horizontal
+        lastRowStackView.spacing = Layout.spacing
+        lastRowStackView.distribution = .fill
+        
+        return lastRowStackView
+    }
+    
+    func setupButtonStack() {
+        buttonStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonStack)
         
         NSLayoutConstraint.activate([
-            verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Layout.sideInset),
-            verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Layout.sideInset),
-            verticalStackView.widthAnchor.constraint(equalTo: verticalStackView.heightAnchor),
-            verticalStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            buttonStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Layout.sideInset),
+            buttonStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Layout.sideInset),
+            buttonStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Layout.bottomInset)
         ])
-        
-        verticalStackView.axis = .vertical
-        verticalStackView.spacing = Layout.spacing
-        verticalStackView.distribution = .fillEqually
-        
     }
 }
 
